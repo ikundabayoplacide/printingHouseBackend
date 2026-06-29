1250c489cd93a3105828737d83fca8b1076bebee
Ikundabayo Placide <ikundabayoplacide500@gmail.com>
2026-06-29 09:52:26 +0200

    modified daf to create employee

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -26,12 +26,12 @@ router.get('/me', getMyProfile);
 router.get('/:id/jobs', getEmployeeJobs);
 router.get('/', getAllEmployees);
 router.get('/:id', getEmployeeById);
-router.post('/', authorize('ADMIN', 'HR'), createEmployeeValidation, validate, createEmployee);
-router.put('/:id', authorize('ADMIN', 'HR'), updateEmployeeValidation, validate, updateEmployee);
-router.patch('/:id/department', authorize('ADMIN', 'HR'), assignDepartment);
-router.patch('/:id/assign-job', authorize('ADMIN', 'HR', 'SUPERVISOR'), assignJobValidation, validate, assignJob);
-router.patch('/:id/unassign-job', authorize('ADMIN', 'HR', 'SUPERVISOR'), assignJobValidation, validate, unassignJob);
-router.patch('/:id/toggle-active', authorize('ADMIN', 'HR'), toggleEmployeeActive);
+router.post('/', authorize('ADMIN', 'HR', 'DAF'), createEmployeeValidation, validate, createEmployee);
+router.put('/:id', authorize('ADMIN', 'HR', 'DAF'), updateEmployeeValidation, validate, updateEmployee);
+router.patch('/:id/department', authorize('ADMIN', 'HR', 'DAF'), assignDepartment);
+router.patch('/:id/assign-job', authorize('ADMIN', 'HR', 'DAF', 'SUPERVISOR'), assignJobValidation, validate, assignJob);
+router.patch('/:id/unassign-job', authorize('ADMIN', 'HR', 'DAF', 'SUPERVISOR'), assignJobValidation, validate, unassignJob);
+router.patch('/:id/toggle-active', authorize('ADMIN', 'HR', 'DAF'), toggleEmployeeActive);
 router.delete('/:id', authorize('ADMIN'), deleteEmployee);
 
 module.exports = router;
